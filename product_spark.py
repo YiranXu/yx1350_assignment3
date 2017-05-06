a92443605b707a69f0110c2659979dd11fdde129
yiran <yx1350@nyu.edu>
2017-05-05 22:49:15 -0400

    modify range

--- product_spark.py
+++ product_spark.py
@@ -2,5 +2,5 @@ from pyspark import SparkContext
 from operator import mul
 if __name__ == '__main__':
 	sc = SparkContext("local", "product")
-	result=sc.parallelize(range(1,1000)).fold(1,mul)
+	result=sc.parallelize(range(1,1001)).fold(1,mul)
 	print("product of all numbers from 1 to 1000: ",result)
--- squareroot_spark.py
+++ squareroot_spark.py
@@ -3,6 +3,6 @@ from operator import mul,add
 import math
 if __name__ == '__main__':
 	sc = SparkContext("local", "product")
-	num=sc.parallelize(range(1,1000)).map(lambda x:math.sqrt(x))
+	num=sc.parallelize(range(1,1001)).map(lambda x:math.sqrt(x))
 	sum=num.fold(0, add)
 	print("average square root of all numbers from 1 to 1000: ",sum/1000)
